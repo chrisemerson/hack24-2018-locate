d4951eb923956815998ef2c0c3fc90020bdf0f64
Chris Emerson <chris@cemerson.co.uk>
2018-03-10 14:32:26 +0000

    Locate API work so far

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+use GuzzleHttp\Client;
+
+require_once "vendor/autoload.php";
+
+const NOTTINGHAM_NORTH = 53.051698;
+const NOTTINGHAM_SOUTH = 52.875512;
+const NOTTINGHAM_EAST = -1.018570;
+const NOTTINGHAM_WEST = -1.337860;
+
+const REGION = 'gb';
+
+const GEOCODE_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+$params = [
+    'region' => REGION,
+    'bounds' => NOTTINGHAM_SOUTH . ',' . NOTTINGHAM_WEST . '|' . NOTTINGHAM_NORTH . ',' . NOTTINGHAM_EAST,
+    'address' => $_GET['place']
+];
+
+$guzzle = new Client();
+
+$result = $guzzle->get(GEOCODE_API_URL, ['query' => $params]);
+
+print_r($result);
